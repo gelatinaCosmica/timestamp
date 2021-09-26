98dabdb7d522c9868e28fd298a002b10f91b42ed
gelatina <santiagozapatabohorquez@hotmail.com>
2021-09-25 20:38:36 -0500

    created date validation with moment

--- server.js
+++ server.js
@@ -4,11 +4,12 @@
 // init project
 var express = require('express');
 var app = express();
+var moment = require('moment')
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+app.use(cors({ optionsSuccessStatus: 200 }));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
@@ -18,15 +19,28 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+  res.json({ greeting: 'hello API' });
 });
 
+app.get("/api/:date", (req, res) => {
+  let isDateValid = moment(req.params.date).isValid()
+  let msDateParam = new Date(Number(req.params.date))
+  let arrDateParam = new Date(req.params.date)
 
+  let dateConvertedObj = {}
+
+  if (isDateValid) {
+    dateConvertedObj = { unix: arrDateParam.getTime(), utc: arrDateParam.toUTCString() }
+  } else {
+    dateConvertedObj = { unix: msDateParam, utc: msDateParam.toUTCString() }
+  }
+
+  res.json(dateConvertedObj)
+})
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
+var listener = app.listen(3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
